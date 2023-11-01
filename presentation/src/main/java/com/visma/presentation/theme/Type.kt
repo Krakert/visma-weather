034119944408217b9a8bb1da77700df2e62fa8d7
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-11-01 14:22:25 +0100

    Update font

--- presentation/src/main/java/com/visma/presentation/theme/Type.kt
+++ presentation/src/main/java/com/visma/presentation/theme/Type.kt
@@ -1,34 +1,54 @@
 package com.visma.presentation.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.visma.weather.presentation.R
+
+
+val LinikSans = FontFamily(
+    Font(resId = R.font.liniksans_light, weight = FontWeight.Light),
+    Font(
+        resId = R.font.liniksans_light_italic,
+        weight = FontWeight.Light,
+        style = FontStyle.Italic
+    ),
+    Font(resId = R.font.liniksans_medium, weight = FontWeight.Medium),
+    Font(
+        resId = R.font.liniksans_medium_italic,
+        weight = FontWeight.Medium,
+        style = FontStyle.Italic
+    ),
+    Font(resId = R.font.liniksans_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.liniksans_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(resId = R.font.liniksans_black, weight = FontWeight.Black),
+    Font(resId = R.font.liniksans_bold_italic, weight = FontWeight.Black, style = FontStyle.Italic)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = LinikSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        color = Color.White
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = LinikSans,
+        fontWeight = FontWeight.Black,
+        fontSize = 48.sp,
+        color = Color.White
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = LinikSans,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 18.sp,
+        color = Color.White.copy(alpha = 0.8F)
     )
-    */
 )
