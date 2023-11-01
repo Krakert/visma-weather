cdc107bec0fcf8c36e84356aed073d631c9b319b
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-11-02 00:43:38 +0100

    Added shimmerEffect

---
+++ presentation/src/main/java/com/visma/presentation/extension/Modifier.kt
@@ -0,0 +1,48 @@
+package com.visma.presentation.extension
+
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
+import com.visma.presentation.theme.BlueDayForecastItem
+import com.visma.presentation.theme.BlueDayForecastRow
+
+fun Modifier.shimmerEffect(): Modifier = composed {
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+    val transition = rememberInfiniteTransition(label = "")
+    val startOffsetX by transition.animateFloat(
+        initialValue = -2 * size.width.toFloat(),
+        targetValue = 2 * size.width.toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(2000)
+        ),
+        label = ""
+    )
+
+    background(
+        brush = Brush.linearGradient(
+            colors = listOf(
+                BlueDayForecastRow.copy(alpha = 0.8F),
+                BlueDayForecastItem.copy(alpha = 0.4F),
+                BlueDayForecastRow.copy(alpha = 0.8F),
+            ),
+            start = Offset(startOffsetX, 0f),
+            end = Offset(startOffsetX + size.width.toFloat(), size.height.toFloat())
+        )
+    ).onGloballyPositioned {
+        size = it.size
+    }
+}
