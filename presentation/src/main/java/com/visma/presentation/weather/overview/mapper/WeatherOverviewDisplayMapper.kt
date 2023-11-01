cd0813a29da0f4396d5061715b27869ded753005
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-11-01 17:34:36 +0100

    Use degreeMapper

--- presentation/src/main/java/com/visma/presentation/weather/overview/mapper/WeatherOverviewDisplayMapper.kt
+++ presentation/src/main/java/com/visma/presentation/weather/overview/mapper/WeatherOverviewDisplayMapper.kt
@@ -1,14 +1,17 @@
 package com.visma.presentation.weather.overview.mapper
 
 import com.visma.domain.weather.model.WeatherReport
+import com.visma.presentation.weather.overview.formatter.DegreeFormatter
 import com.visma.presentation.weather.overview.model.WeatherOverviewDisplay
 import javax.inject.Inject
 import kotlin.math.roundToInt
 
-class WeatherOverviewDisplayMapper @Inject constructor() {
+class WeatherOverviewDisplayMapper @Inject constructor(
+    private val degreeFormatter: DegreeFormatter
+) {
     fun map (weather: WeatherReport): WeatherOverviewDisplay {
         return WeatherOverviewDisplay(
-            temperature = weather.temperature.roundToInt().toString(),
+            temperature = degreeFormatter.map(weather.temperature.roundToInt().toString()),
             icon = weather.icon,
             description = weather.description
         )
