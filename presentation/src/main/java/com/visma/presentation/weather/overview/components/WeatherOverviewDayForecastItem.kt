5eef75911b90d72083aad897d1ed72430c1e5cf1
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-11-02 00:49:01 +0100

    Made icon bigger

--- presentation/src/main/java/com/visma/presentation/weather/overview/components/WeatherOverviewDayForecastItem.kt
+++ presentation/src/main/java/com/visma/presentation/weather/overview/components/WeatherOverviewDayForecastItem.kt
@@ -3,12 +3,9 @@ package com.visma.presentation.weather.overview.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -21,15 +18,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.visma.presentation.theme.BlueDayForecastItem
-import com.visma.presentation.theme.BlueDayForecastRow
-import com.visma.presentation.theme.LocalShapes
 import com.visma.presentation.theme.dimensions
-import com.visma.presentation.weather.overview.formatter.DegreeFormatter
-import com.visma.presentation.weather.overview.model.WeatherForecast24hItemDisplay
 
 @Composable
 fun WeatherDayForecastItem(time: String, icon: String, temp: String) {
-
     Box(
         modifier = Modifier.padding(
             horizontal = MaterialTheme.dimensions.spacingMedium,
@@ -44,7 +36,7 @@ fun WeatherDayForecastItem(time: String, icon: String, temp: String) {
                     horizontal = MaterialTheme.dimensions.spacingMedium,
                     vertical = MaterialTheme.dimensions.spacingExtraLarge
                 )
-                .width(65.dp), contentAlignment = Alignment.Center
+                .width(75.dp), contentAlignment = Alignment.Center
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -59,7 +51,7 @@ fun WeatherDayForecastItem(time: String, icon: String, temp: String) {
                     model = icon,
                     contentDescription = "",
                     contentScale = ContentScale.Crop,
-                    modifier = Modifier.size(48.dp)
+                    modifier = Modifier.size(72.dp)
                 )
                 Text(
                     modifier = Modifier.padding(
@@ -76,55 +68,4 @@ fun WeatherDayForecastItem(time: String, icon: String, temp: String) {
 @Composable
 fun PreviewWeatherDayForecastItem() {
     WeatherDayForecastItem("9AM", "", "16")
-}
-
-@Preview
-@Composable
-fun PreviewWeatherDayForecastRow() {
-    WeatherDayForecastRow(
-        listOf(
-            WeatherForecast24hItemDisplay("9AM", "",  DegreeFormatter().map("16")),
-            WeatherForecast24hItemDisplay("10AM", "", DegreeFormatter().map("17")),
-            WeatherForecast24hItemDisplay("11AM", "", DegreeFormatter().map("18")),
-            WeatherForecast24hItemDisplay("12AM", "", DegreeFormatter().map("19")),
-            WeatherForecast24hItemDisplay("1PM", "", DegreeFormatter().map("22")),
-            WeatherForecast24hItemDisplay("2PM", "", DegreeFormatter().map("21")),
-            WeatherForecast24hItemDisplay("3PM", "", DegreeFormatter().map("18")),
-            WeatherForecast24hItemDisplay("4PM", "", DegreeFormatter().map("16")),
-            WeatherForecast24hItemDisplay("5PM", "", DegreeFormatter().map("15")),
-            WeatherForecast24hItemDisplay("6PM", "", DegreeFormatter().map("13"))
-        )
-    )
-}
-
-@Composable
-fun WeatherDayForecastRow(forecast: List<WeatherForecast24hItemDisplay>) {
-    LazyRow(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        item {
-            Box(modifier = Modifier.padding(start = 54.dp, end = 32.dp)) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(LocalShapes.current.forecastRowShape)
-                        .background(BlueDayForecastRow)
-                        .padding(
-                            horizontal = MaterialTheme.dimensions.spacingExtraLarge,
-                            vertical = MaterialTheme.dimensions.spacingExtraLarge
-                        )
-                ) {
-                    Row(
-                    ) {
-                        forecast.forEach {
-                            WeatherDayForecastItem(
-                                time = it.time, icon = it.icon, temp = it.temperature
-                            )
-                        }
-                    }
-                }
-            }
-        }
-    }
-
 }
