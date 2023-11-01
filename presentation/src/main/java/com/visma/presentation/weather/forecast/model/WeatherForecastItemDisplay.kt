94d5ec003ea6132a255fc871e588f17683665e3d
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-11-02 00:47:00 +0100

    Not needed anymore

--- presentation/src/main/java/com/visma/presentation/weather/forecast/model/WeatherForecastItemDisplay.kt
+++
@@ -1,8 +0,0 @@
-package com.visma.presentation.weather.forecast.model
-
-data class WeatherForecastItemDisplay (
-    val time: String,
-    val icon: String,
-    val temperature: String,
-    val rain: String,
-)
--- presentation/src/main/java/com/visma/presentation/weather/overview/model/WeatherForecast24hItemDisplay.kt
+++
@@ -1,7 +0,0 @@
-package com.visma.presentation.weather.overview.model
-
-data class WeatherForecast24hItemDisplay (
-    val time: String,
-    val icon: String,
-    val temperature: String
-)
