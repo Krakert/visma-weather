84507f14b6af7b154ddabcdf0b3875671a57148a
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-11-02 00:50:17 +0100

    Added V0.1 layout | WIP

--- presentation/src/main/java/com/visma/presentation/weather/forecast/WeatherForecastScreen.kt
+++ presentation/src/main/java/com/visma/presentation/weather/forecast/WeatherForecastScreen.kt
@@ -2,25 +2,35 @@ package com.visma.presentation.weather.forecast
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import com.visma.presentation.OnDisplay
+import com.visma.presentation.OnError
+import com.visma.presentation.OnLoading
 import com.visma.presentation.theme.BlackForecastScreen
 import com.visma.presentation.theme.GreyForecastScreen
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WeatherForecastScreen(
     navController: NavHostController,
@@ -28,7 +38,9 @@ fun WeatherForecastScreen(
     city: String
 ) {
 
-    LaunchedEffect(Unit){
+    val contentForecast by viewModel.weatherOverviewForecast.collectAsState()
+
+    LaunchedEffect(Unit) {
         viewModel.fetchWeatherForecast(city)
     }
 
@@ -50,10 +62,63 @@ fun WeatherForecastScreen(
         Text(
             text = "Forecast next 14 days",
             style = MaterialTheme.typography.displayLarge.copy(
-                color = BlackForecastScreen,
-                fontWeight = FontWeight.Black
+                color = BlackForecastScreen
             )
         )
+
+        when (val state = contentForecast) {
+            is OnDisplay -> {
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    state.display.result.forEach {
+                        item {
+                            Text(
+                                text = it.date, style = MaterialTheme.typography.displayLarge.copy(
+                                    BlackForecastScreen
+                                )
+                            )
+                            LazyColumn(modifier = Modifier.height(100.dp)) {
+                                it.forecasts.forEach {
+                                    item {
+                                        Row {
+                                            Text(
+                                                text = it.time,
+                                                style = MaterialTheme.typography.labelSmall.copy(
+                                                    BlackForecastScreen
+                                                )
+                                            )
+                                            Spacer(modifier = Modifier.weight(1f))
+                                            Row(horizontalArrangement = Arrangement.spacedBy(10.dp),
+                                                verticalAlignment = Alignment.CenterVertically) {
+
+                                                AsyncImage(
+                                                    model = it.icon,
+                                                    contentDescription = "",
+                                                    contentScale = ContentScale.Fit,
+                                                    modifier = Modifier.size(48.dp)
+                                                )
+                                                Text(
+                                                    text = it.temperature,
+                                                    style = MaterialTheme.typography.labelSmall.copy(
+                                                        BlackForecastScreen
+                                                    )
+                                                )
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            is OnError -> {
+            }
+
+            OnLoading -> {
+            }
+        }
     }
 
 }
