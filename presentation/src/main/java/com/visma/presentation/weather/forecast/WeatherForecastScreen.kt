d3903829bb2dd3780f6ad9b29d2d8ed8f7b80df7
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-11-02 11:27:46 +0100

    Update layout

--- presentation/src/main/java/com/visma/presentation/weather/forecast/WeatherForecastScreen.kt
+++ presentation/src/main/java/com/visma/presentation/weather/forecast/WeatherForecastScreen.kt
@@ -1,35 +1,34 @@
 package com.visma.presentation.weather.forecast
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import coil.compose.AsyncImage
 import com.visma.presentation.OnDisplay
 import com.visma.presentation.OnError
 import com.visma.presentation.OnLoading
-import com.visma.presentation.theme.BlackForecastScreen
-import com.visma.presentation.theme.GreyForecastScreen
+import com.visma.presentation.extension.shimmerEffect
+import com.visma.presentation.theme.BlueGradientBottom
+import com.visma.presentation.theme.BlueGradientTop
+import com.visma.presentation.theme.LocalDimensions
+import com.visma.presentation.theme.dimensions
+import com.visma.presentation.weather.forecast.components.WeatherForecastTopBar
+import com.visma.presentation.weather.overview.components.WeatherDayForecastRow
 
 @Composable
 fun WeatherForecastScreen(
@@ -43,80 +42,55 @@ fun WeatherForecastScreen(
     LaunchedEffect(Unit) {
         viewModel.fetchWeatherForecast(city)
     }
-
-    Column(
+    Box(
         modifier = Modifier
-            .padding(18.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(BlueGradientTop, BlueGradientBottom)
+                )
+            )
     ) {
-        IconButton(modifier = Modifier.size(54.dp),
-            onClick = { navController.navigateUp() }) {
-            Icon(
-                imageVector = Icons.Filled.ArrowBack,
-                tint = GreyForecastScreen,
-                contentDescription = "Back",
-                modifier = Modifier
-                    .size(32.dp)
-            )
-        }
-        Text(
-            text = "Forecast next 14 days",
-            style = MaterialTheme.typography.displayLarge.copy(
-                color = BlackForecastScreen
-            )
-        )
 
-        when (val state = contentForecast) {
-            is OnDisplay -> {
-                LazyColumn(modifier = Modifier.fillMaxSize()) {
-                    state.display.result.forEach {
-                        item {
-                            Text(
-                                text = it.date, style = MaterialTheme.typography.displayLarge.copy(
-                                    BlackForecastScreen
-                                )
-                            )
-                            LazyColumn(modifier = Modifier.height(100.dp)) {
-                                it.forecasts.forEach {
-                                    item {
-                                        Row {
-                                            Text(
-                                                text = it.time,
-                                                style = MaterialTheme.typography.labelSmall.copy(
-                                                    BlackForecastScreen
-                                                )
-                                            )
-                                            Spacer(modifier = Modifier.weight(1f))
-                                            Row(horizontalArrangement = Arrangement.spacedBy(10.dp),
-                                                verticalAlignment = Alignment.CenterVertically) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimensions.spacingLarge),
+        ) {
+            WeatherForecastTopBar {
+                navController.navigateUp()
+            }
+            when (val state = contentForecast) {
+                is OnDisplay -> {
+                    LazyColumn(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(bottom = MaterialTheme.dimensions.spacingLarge)) {
+                        state.display.result.forEach {
+                            item {
+                                Box(modifier = Modifier.padding(MaterialTheme.dimensions.spacingMedium)) {
+                                    Text(
+                                        modifier = Modifier.padding(start = LocalDimensions.current.spacingForecastRow),
+                                        text = it.date,
+                                        style = MaterialTheme.typography.displayLarge
 
-                                                AsyncImage(
-                                                    model = it.icon,
-                                                    contentDescription = "",
-                                                    contentScale = ContentScale.Fit,
-                                                    modifier = Modifier.size(48.dp)
-                                                )
-                                                Text(
-                                                    text = it.temperature,
-                                                    style = MaterialTheme.typography.labelSmall.copy(
-                                                        BlackForecastScreen
-                                                    )
-                                                )
-                                            }
-                                        }
-                                    }
+                                    )
                                 }
+                                WeatherDayForecastRow(false, it.forecasts)
                             }
-
                         }
                     }
                 }
-            }
 
-            is OnError -> {
-            }
+                is OnError -> {
+                }
 
-            OnLoading -> {
+                OnLoading -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(start = LocalDimensions.current.spacingForecastRow)
+                            .clip(RoundedCornerShape(topStart = 24.dp, bottomStart = 24.dp))
+                            .shimmerEffect()
+                    )
+                }
             }
         }
     }
---
+++ presentation/src/main/java/com/visma/presentation/weather/forecast/components/WeatherForecastTopBar.kt
@@ -0,0 +1,42 @@
+package com.visma.presentation.weather.forecast.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.visma.presentation.theme.LocalDimensions
+import com.visma.presentation.theme.dimensions
+import com.visma.weather.presentation.R
+
+@Composable
+fun WeatherForecastTopBar(onClick: () -> Unit) {
+    Box(modifier = Modifier.padding(MaterialTheme.dimensions.spacingLarge)) {
+        Column(verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimensions.spacingLarge)) {
+            IconButton(modifier = Modifier.size(LocalDimensions.current.spacingForecastRow),
+                onClick = { onClick() }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    tint = Color.White,
+                    contentDescription = "Back",
+                    modifier = Modifier.size(32.dp)
+                )
+            }
+            Text(
+                text = stringResource(R.string.txt_forecast_14d),
+                style = MaterialTheme.typography.displayLarge
+            )
+        }
+    }
+}
