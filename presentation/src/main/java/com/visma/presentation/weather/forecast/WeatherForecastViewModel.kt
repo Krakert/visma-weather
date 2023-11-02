9616459d023023a5c21c1da198936a1d743047ce
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-11-02 11:28:01 +0100

    small delay added for shimmer

--- presentation/src/main/java/com/visma/presentation/weather/forecast/WeatherForecastViewModel.kt
+++ presentation/src/main/java/com/visma/presentation/weather/forecast/WeatherForecastViewModel.kt
@@ -10,6 +10,7 @@ import com.visma.presentation.OnLoading
 import com.visma.presentation.weather.forecast.mapper.WeatherForecastDisplayMapper
 import com.visma.presentation.weather.forecast.model.WeatherForecastListDisplay
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -27,6 +28,7 @@ class WeatherForecastViewModel @Inject constructor(
     fun fetchWeatherForecast(city: String) {
         viewModelScope.launch {
             getForecastByCity(city, 40).onSuccess {
+                delay(1000)
                 mutableForecastFlow.emit(
                     OnDisplay(weatherForecastDisplayMapper.map(it))
                 )
